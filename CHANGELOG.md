800a8ffab6e9b3a19ead1dc4892c1fcfa042d316
Laisky <ppcelery@gmail.com>
2016-01-08 14:38:09 +0800

    docs: use commitizen

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,14 +1,6 @@
-Versions Log
-===
+<a name="1.1.1"></a>
+## 1.1.1 (2016-01-08)
 
-- 2.0
 
-|Version|Description|Date|
-|:--:|:--:|:--:|
-| 1.1 | Refactor the method to register task | Mon Dec 28 02:41:16 2015 UTC |
 
-- 1.0
 
-|Version|Description|Date|
-|:--:|:--:|:--:|
-| 1.0 | 具备基础的 task 功能 | Sun Dec 27 09:34:18 2015 UTC |
--- setup.py
+++ setup.py
@@ -1,33 +1,8 @@
-from setuptools import setup, find_packages
-from pip.req import parse_requirements
-from pip.download import PipSession
+#!/usr/bin/env python
 
+from setuptools import setup
 
-requires = [str(i.req) for i in parse_requirements('requirements.txt',
-                                                   session=PipSession())
-            if i.req is not None]
-
-
-setup(name='ramjet',
-      version='1.1.1',
-      author="Laisky",
-      author_email="ppcelery@gmail.com",
-      description="Timing tasks framework.",
-      license="MIT/Apache",
-      url="https://github.com/Laisky/ramjet",
-      keywords="timing tasks frameworks asynchrous",
-      packages=find_packages('src'),
-      package_dir={'': 'src'},
-      include_package_data=True,
-      install_requires=requires,
-      classifiers=[
-          "Development Status :: 4 - Beta",
-          'Topic :: Software Development :: Libraries',
-          "Environment :: Web Environment",
-          "Intended Audience :: Developers",
-          "License :: OSI Approved :: MIT License",
-          "Operating System :: OS Independent",
-          "Programming Language :: Python",
-          'Programming Language :: Python :: 3.4',
-      ],
-      zip_safe=True)
+setup(
+    setup_requires=['pbr'],
+    pbr=True,
+)
