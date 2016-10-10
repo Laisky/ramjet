0417a079a5740284494c9acc91fd023e600c30ba
Laisky <ppcelery@gmail.com>
2016-10-10 16:58:30 +0800

    feat: 增加 http handler
    task 可以监听 http 请求
    
    closes #3

--- ramjet/__main__.py
+++ ramjet/__main__.py
@@ -2,10 +2,11 @@ import logging
 
 import tornado.options as opt
 from tornado.options import options
+from aiohttp import web
 
-from ramjet.app import Application
+
+from ramjet.app import setup_web_handlers
 from ramjet.settings import LOG_NAME
-from ramjet.engines import ioloop
 
 
 log = logging.getLogger(LOG_NAME)
@@ -13,10 +14,6 @@ log = logging.getLogger(LOG_NAME)
 
 def main():
     opt.parse_command_line()
-
-    # 暂不需要 Web 应用
-    # http_server = tornado.httpserver.HTTPServer(Application())
-    # http_server.listen(options.port)
 
     if options.debug:
         log.info('start application in debug mode')
@@ -26,9 +23,11 @@ def main():
         log.setLevel(logging.INFO)
 
     from ramjet.tasks import setup_tasks
-    setup_tasks(ioloop)
 
-    ioloop.run_forever()
+    app = web.Application()
+    setup_tasks(app)
+    setup_web_handlers(app)
+    web.run_app(app, host='localhost', port=options.port)
 
 if __name__ == '__main__':
     main()
