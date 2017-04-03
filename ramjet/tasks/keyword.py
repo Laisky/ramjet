aff9e9797e468e878f53af771b4277a727fb0411
Laisky <ppcelery@gmail.com>
2017-04-03 23:23:14 +0800

    feat: loads keywords for each post

--- ramjet/tasks/keyword.py
+++ ramjet/tasks/keyword.py
@@ -10,7 +10,7 @@ from ramjet.engines import ioloop, process_executor
 from ramjet.utils import utcnow, get_conn
 
 
-logger = logger.getChild('tasks.heart')
+logger = logger.getChild('tasks.keyword')
 
 N_CHINESE_KEYWORDS = 30
 N_ENGLISH_KEYWORDS = 20
@@ -24,28 +24,44 @@ def bind_task():
         later = 60 * 60 * 12  # 12 hr
         logger.debug('add new task load_keywords after {}'.format(later))
         logger.info('Run task load_keywords')
-        process_executor.submit(load_keywords)
+        process_executor.submit(load_and_save_post_cnt_and_keywords)
         ioloop.call_later(later, callback)
 
     ioloop.call_later(1, callback)
 
 
-def load_keywords():
+def load_and_save_post_cnt_and_keywords():
+    conn = get_conn()
+    db = conn.blog
+    coll = db[DB]
+
+    all_cnt = ''
+    for docu in db.posts.find({'post_password': ''}):
+        cnt = docu['post_content']
+        k_cn_each = load_chinese_keyword(cnt)
+        k_en_each = load_english_keyword(cnt)
+        if k_en_each and k_cn_each:
+            tags = k_cn_each[:3] + k_en_each[:2]
+        elif k_cn_each:
+            tags = k_cn_each[:5]
+        elif k_en_each:
+            tags = k_en_each[:5]
+
+        db.posts.update_one({'_id': docu['_id']},
+                            {'$set': {'post_tags': tags}})
+        logger.debug('update %s\'s post_tags: %s', docu.get('post_title'), tags)
+        all_cnt += '\t' + cnt
+
+    load_keywords_for_all(coll, all_cnt)
+
+
+def load_keywords_for_all(coll, all_cnt):
     logger.debug('load_keywords')
 
     try:
-        conn = get_conn()
-        db = conn.blog
-        coll = db[DB]
-
-        contents = []
-        for docu in db.posts.find({'post_password': ''}, {'post_content': 1}):
-            contents.append(docu['post_content'])
-
-        c = '\t'.join(contents)
-        k_cn = load_chinese_keyword(c)
+        k_cn = load_chinese_keyword(all_cnt)
         logger.debug('load chinese keywords: {}'.format(k_cn))
-        k_en = load_english_keyword(c)
+        k_en = load_english_keyword(all_cnt)
         logger.debug('load english keywords: {}'.format(k_en))
         keywords = [] + k_cn + k_en
 
@@ -82,7 +98,8 @@ def load_english_keyword(content):
         'pnumber', 'subject', 'alignnone', 'ppcelery', 'color', 'config', 'shell',
         'static', 'index', 'label', 'number', 'param', 'users', 'first', 'notebook',
         'write', 'volume', 'files', 'total', 'timeit', 'latest', 'which', 'login',
-        'field', 'requesthandler', 'https',
+        'field', 'requesthandler', 'https', 'tbody', 'table', 'thead', 'white', 'border',
+        'cellpadding', 'wordpress', 'archives', 'nofollow', 'small', 'before', 'should',
     ])
     key_map = defaultdict(int)
     kws = jieba.cut(content, cut_all=False)
@@ -100,4 +117,4 @@ def load_english_keyword(content):
 
 
 if __name__ == '__main__':
-    load_keywords()
+    load_and_save_post_cnt_and_keywords()
