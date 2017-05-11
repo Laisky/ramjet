a5c5c19007cf0b33b78a4c4cd9e7a785ea46e55a
Laisky <ppcelery@gmail.com>
2017-05-11 15:59:21 +0800

    fix: add more stopwords to keyword

--- ramjet/tasks/keyword.py
+++ ramjet/tasks/keyword.py
@@ -17,6 +17,34 @@ N_ENGLISH_KEYWORDS = 20
 DB_HOST = 'localhost'
 DB_PORT = 27016
 DB = 'statistics'
+STOP_WORDS = set([
+    'strong', 'class', 'style', 'uploads', 'align', 'height', 'image',
+    'center', 'aligncenter', 'target', 'blockquote', 'blank', 'response',
+    'width', 'align', 'alignleft', 'attachment', 'password', 'login',
+    'valign', 'content', 'value', 'print', 'import', 'datetime', 'title', 'padding',
+    'laisky', 'return', 'images', 'start', 'about', 'cinderellananako',
+    'pnumber', 'subject', 'alignnone', 'ppcelery', 'color', 'config', 'shell',
+    'static', 'index', 'label', 'number', 'param', 'users', 'first', 'notebook',
+    'write', 'volume', 'files', 'total', 'timeit', 'latest', 'which', 'login',
+    'field', 'requesthandler', 'https', 'tbody', 'table', 'thead', 'white', 'border',
+    'cellpadding', 'wordpress', 'archives', 'nofollow', 'small', 'before', 'should',
+    'i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', 'your', 'yours', 'yourself',
+    'yourselves', 'he', 'him', 'his', 'himself', 'she', 'her', 'hers', 'herself', 'it', 'its', 'itself',
+    'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', 'these',
+    'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having',
+    'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as',
+    'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into',
+    'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in',
+    'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when',
+    'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some',
+    'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't',
+    'can', 'will', 'just', 'don', 'should', 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain',
+    'aren', 'couldn', 'didn', 'doesn', 'hadn', 'hasn', 'haven', 'isn', 'ma', 'mightn', 'mustn',
+    'needn', 'shan', 'shouldn', 'wasn', 'weren', 'won', 'wouldn', 'http', 'br', 'li', 'div', 'tag',
+    'table', 'td', 'tr', 'text', 'left', 'html', 'href', 'blog', 'sina', 'size', 'nbsp', 'span',
+    'weibo', 'youku', 'douban', 'bgyx', 'tumblr', 'full', 'error', 'null', 'true', 'false', 'qiniu', 'self',
+])
+
 
 
 def bind_task():
@@ -91,24 +119,12 @@ def load_chinese_keyword(content):
 def load_english_keyword(content):
     logger.debug('load_english_keyword')
 
-    eng_kw_regex = re.compile('[a-zA-Z]{5,10}')
-    ignores = set([
-        'strong', 'class', 'style', 'uploads', 'align', 'height', 'image',
-        'center', 'aligncenter', 'target', 'blockquote', 'blank', 'response',
-        'width', 'align', 'alignleft', 'attachment', 'password', 'login',
-        'valign', 'content', 'value', 'print', 'import', 'datetime', 'title', 'padding',
-        'laisky', 'return', 'images', 'start', 'about', 'cinderellananako',
-        'pnumber', 'subject', 'alignnone', 'ppcelery', 'color', 'config', 'shell',
-        'static', 'index', 'label', 'number', 'param', 'users', 'first', 'notebook',
-        'write', 'volume', 'files', 'total', 'timeit', 'latest', 'which', 'login',
-        'field', 'requesthandler', 'https', 'tbody', 'table', 'thead', 'white', 'border',
-        'cellpadding', 'wordpress', 'archives', 'nofollow', 'small', 'before', 'should',
-    ])
+    eng_kw_regex = re.compile('^[a-zA-Z]{4,15}$')
     key_map = defaultdict(int)
     kws = jieba.cut(content, cut_all=False)
     for k in kws:
         k = k.strip().lower()
-        if k in ignores or not eng_kw_regex.match(k):
+        if k in STOP_WORDS or not eng_kw_regex.match(k):
             continue
 
         key_map[k] += 1
