7367d9eecc85fe59612bff2f17c2bc694f5bff5f
Laisky <ppcelery@gmail.com>
2018-05-17 09:54:22 +0800

    fix: cyclicity gc

--- ramjet/tasks/heart.py
+++ ramjet/tasks/heart.py
@@ -1,6 +1,8 @@
 """Task 示例
 """
 
+import gc
+
 from ramjet.settings import logger
 from ramjet.engines import ioloop
 
@@ -10,6 +12,7 @@ logger = logger.getChild('tasks.heart')
 def bind_task():
     def callback(*args, **kw):
         logger.info('tasks heart!')
+        gc.collect()
         ioloop.call_later(60, callback, *args, **kw)
 
     ioloop.call_later(0, callback)
