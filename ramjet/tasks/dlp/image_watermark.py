80d941b1078ad79d5e8d69f5e845952a2b774256
Laisky.Cai <ppcelery@gmail.com>
2021-03-12 17:07:12 +0800

    feat: dlp image watermark;

--- ramjet/tasks/dlp/image_watermark.py
+++ ramjet/tasks/dlp/image_watermark.py
@@ -1,10 +1,12 @@
 import json
 import os
 import tempfile
-from base64 import b64encode
+from asyncio import wait
+from base64 import urlsafe_b64encode
 from hashlib import md5
 from uuid import uuid1
 
+import aiohttp
 import aiohttp_jinja2
 import requests
 from aiohttp import web
@@ -12,21 +14,24 @@ from ramjet.settings import (TENCENT_CLOUD_SIGNATURE,
                              TENCENT_CLOUD_WATERMARK_BUCKET_URL, logger)
 
 
-class ImageWaterMarkView(web.View):
+class ImageWaterMarkSignView(web.View):
     async def get(self):
         """get watermarked image
 
         `/dlp/image/watermark/?url=xxx&text=xxx`
         """
         img_remote_url = self.request.rel_url.query["url"]
-        watermark = b64encode(self.request.rel_url.query["text"].encode("utf8"))
+        watermark = self.request.rel_url.query["text"]
+
         hasher = md5()
         hasher.update(img_remote_url.encode("utf8"))
         key = hasher.hexdigest()
 
-        img_url = tencent_cloud.fetch_and_upload_image(img_remote_url, key)
-        redirect_to = f"{img_url}?watermark/3/type/3/text/{watermark}"
-        raise web.HTTPFound(redirect_to)
+        imgdata = await tencent_cloud.fetch_and_sign_image(
+            img_remote_url, key, watermark
+        )
+        return web.Response(headers={"content-type": "image/jpeg"}, body=imgdata)
+
 
 class ImageWaterMarkView(web.View):
     @aiohttp_jinja2.template("dlp/image_watermark_doc.html")
@@ -45,67 +50,96 @@ class ImageWaterMarkVerifyView(web.View):
 
     async def post(self):
         data = await self.request.post()
-        print(data.keys())
-        watermark_url=tencent_cloud.load_watermarked_image_url(data['image'])
-        raise web.HTTPFound(watermark_url)
+        imgdata = await tencent_cloud.extract_watermark(data["image"])
+        return web.Response(headers={"content-type": "image/jpeg"}, body=imgdata)
 
 
 class TencentCloud:
     _client = None
 
     def __init__(self):
-        self._client = requests.Session()
+        self._client = aiohttp.ClientSession()
         self._client.headers.update(
             {
                 "Authorization": TENCENT_CLOUD_SIGNATURE,
             }
         )
 
-    def fetch_and_upload_image(self, img_url: str, key: str) -> str:
-        resp = self._client.get(img_url)
-        assert resp.status_code == 200, f"got resp [{resp.status_code}]: {resp.text}"
+    async def fetch_and_sign_image(
+        self, img_url: str, key: str, watermark: str
+    ) -> bytearray:
+        resp = await self._client.get(img_url)
+        assert resp.status == 200, f"got resp [{resp.status}]: {await resp.text()}"
 
         logger.info(f"downloaded image {img_url=}")
         with tempfile.TemporaryFile() as fp:
-            fp.write(resp.content)
+            fp.write(await resp.read())
             fp.seek(0, 0)
 
-            resp = self._client.put(
+            resp = await self._client.put(
                 url=f"{TENCENT_CLOUD_WATERMARK_BUCKET_URL}/{key}", data=fp.read()
             )
-            assert (
-                resp.status_code == 200
-            ), f"got resp [{resp.status_code}]: {resp.text}"
+            assert resp.status == 200, f"got resp [{resp.status}]: {await resp.text()}"
 
             logger.info(f"uploaded image {img_url=}")
-            return f"{TENCENT_CLOUD_WATERMARK_BUCKET_URL}/{key}"
+            img_tmp_url = f"{TENCENT_CLOUD_WATERMARK_BUCKET_URL}/{key}"
 
-    def load_watermarked_image_url(self, imgfile) -> str:
+            watermark = urlsafe_b64encode(watermark.encode("utf8")).decode("utf8")
+            resp = await self._client.get(
+                f"{img_tmp_url}?watermark/3/type/3/text/{watermark}"
+            )
+            assert (
+                resp.status == 200
+            ), f"download signed image [{resp.status}]: {await resp.text()}"
+
+            await self._client.delete(img_tmp_url)
+
+            return await resp.read()
+
+    async def extract_watermark(self, imgfile) -> bytearray:
         # upload image to cloud
         fname = f"{uuid1()}.{os.path.splitext(imgfile.filename)[1]}"
         upload_key = f"/verify/{fname}"
         extract_key = f"/extracted/{fname}"
-        resp = self._client.put(
+        resp = await self._client.put(
             url=f"{TENCENT_CLOUD_WATERMARK_BUCKET_URL}{upload_key}",
             data=imgfile.file.read(),
             headers={
                 "Pic-Operations": json.dumps(
-                        {
-                            "is_pic_info": 0,
-                            "rules": [
-                                {
-                                    "fileid": extract_key,
-                                    "rule": "watermark/4/type/3/text/xxx",
-                                }
-                            ],
-                        }
+                    {
+                        "is_pic_info": 0,
+                        "rules": [
+                            {
+                                "fileid": extract_key,
+                                "rule": "watermark/4/type/3/text/xxx",
+                            }
+                        ],
+                    }
                 )
             },
         )
-        assert resp.status_code == 200, f"got resp [{resp.status_code}]: {resp.text}"
+        assert (
+            resp.status == 200
+        ), f"upload image to extract watermark [{resp.status}]: {await resp.text()}"
 
         logger.info(f"uploaded verify image {fname=}")
-        return f"{TENCENT_CLOUD_WATERMARK_BUCKET_URL}{extract_key}"
+        watermark_url = f"{TENCENT_CLOUD_WATERMARK_BUCKET_URL}{extract_key}"
+
+        resp = await self._client.get(watermark_url)
+        assert (
+            resp.status == 200
+        ), f"download watermark [{resp.status}]: {await resp.text()}"
+
+        await wait(
+            [
+                self._client.delete(
+                    f"{TENCENT_CLOUD_WATERMARK_BUCKET_URL}{upload_key}"
+                ),
+                self._client.delete(f"{watermark_url}"),
+            ]
+        )
+
+        return await resp.read()
 
 
 tencent_cloud = TencentCloud()
--- ramjet/tasks/dlp/router.py
+++ ramjet/tasks/dlp/router.py
@@ -1,7 +1,8 @@
-from .image_watermark import ImageWaterMarkVerifyView, ImageWaterMarkView
+from .image_watermark import (ImageWaterMarkSignView, ImageWaterMarkVerifyView,
+                              ImageWaterMarkView)
 
 
 def bind_handle(add_route):
-    add_route("image/watermark/fetch/", ImageWaterMarkView)
+    add_route("image/watermark/fetch/", ImageWaterMarkSignView)
     add_route("image/watermark/verify/", ImageWaterMarkVerifyView)
     add_route("image/watermark/", ImageWaterMarkView)
