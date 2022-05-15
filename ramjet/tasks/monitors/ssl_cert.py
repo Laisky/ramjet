8e66ed6b1e0332e81fb9e3c21829f460161d75b7
Laisky.Cai <public@laisky.com>
2022-05-15 09:49:19 +0000

    feat: save files to s3

--- ramjet/tasks/monitors/ssl_cert.py
+++ ramjet/tasks/monitors/ssl_cert.py
@@ -2,53 +2,74 @@ import socket
 import datetime
 import ssl
 from textwrap import dedent
+from collections import namedtuple
 
 from ramjet.engines import ioloop, thread_executor
 from ramjet.utils import send_mail
 from .base import logger as monitor_logger
 
+_SiteConf = namedtuple("_SiteConf", ["domain", "check_ssl"])
 
-ALERT_RECEIVERS = ['ppcelery@gmail.com']
-DOMAINS = [
-    'blog.laisky.com',
+
+ALERT_RECEIVERS = ["ppcelery@gmail.com"]
+SITES = [
+    _SiteConf(domain="blog.laisky.com", check_ssl=True),
 ]
 
 
-logger = monitor_logger.getChild('ssl_cert')
+logger = monitor_logger.getChild("ssl_cert")
 
 
 def bind_task():
     def run():
-        logger.info('running...')
+        logger.info("running...")
         thread_executor.submit(main)
-        ioloop.call_later(24*3600, run)
+        ioloop.call_later(24 * 3600, run)
 
     run()
 
 
 def main():
     try:
-        for domain in DOMAINS:
-            expired_at = load_ssl_expiry_datetime(domain)
-            if is_need_to_alert(expired_at):
-                send_alert_email(domain, expired_at)
+        map(checker, SITES)
     except Exception as err:
         logger.exception(err)
 
 
+def checker(site: _SiteConf) -> None:
+    check_ssl(site)
+
+
+def check_ssl(site: _SiteConf) -> None:
+    if not site.check_ssl:
+        return
+
+    expired_at = load_ssl_expiry_datetime(site.domain)
+    if is_ssl_cert_need_alert(expired_at):
+        send_alert_email(site.domain, expired_at)
+
+
 def send_alert_email(domain, expired_at):
-    logger.info('send_alert_email for domain %s', domain)
-    content = dedent('''
+    logger.info("send_alert_email for domain %s", domain)
+    content = dedent(
+        """
         These domains' cert is goging to expired!
 
         {domain}: will expired at {expired_at}
-    '''.format(domain=domain, expired_at=expired_at))
-    send_mail(to_addrs=ALERT_RECEIVERS, subject='HTTPS cert going to expired!', content=content)
+    """.format(
+            domain=domain, expired_at=expired_at
+        )
+    )
+    send_mail(
+        to_addrs=ALERT_RECEIVERS,
+        subject="HTTPS cert going to expired!",
+        content=content,
+    )
 
 
 def load_ssl_expiry_datetime(hostname):
-    logger.debug('load_ssl_expiry_datetime for hostname: %s', hostname)
-    ssl_date_fmt = r'%b %d %H:%M:%S %Y %Z'
+    logger.debug("load_ssl_expiry_datetime for hostname: %s", hostname)
+    ssl_date_fmt = r"%b %d %H:%M:%S %Y %Z"
 
     context = ssl.create_default_context()
     conn = context.wrap_socket(
@@ -60,13 +81,14 @@ def load_ssl_expiry_datetime(hostname):
 
     conn.connect((hostname, 443))
     ssl_info = conn.getpeercert()
+    assert ssl_info is not None
     # parse the string from the certificate into a Python datetime object
-    return datetime.datetime.strptime(ssl_info['notAfter'], ssl_date_fmt)
+    return datetime.datetime.strptime(ssl_info["notAfter"], ssl_date_fmt)
 
 
-def is_need_to_alert(valid_to):
+def is_ssl_cert_need_alert(valid_to) -> bool:
     return (valid_to - datetime.datetime.utcnow()) < datetime.timedelta(days=7)
 
 
-if __name__ == '__main__':
-    print(load_ssl_expiry_datetime('laisky.com'))
+if __name__ == "__main__":
+    print(load_ssl_expiry_datetime("laisky.com"))
---
+++ ramjet/tasks/twitter/s3.py
@@ -0,0 +1,72 @@
+"""upload files to s3
+"""
+
+import os
+from typing import Generator
+
+import boto3
+import botocore
+from botocore.exceptions import ClientError
+from kipp.utils import setup_logger
+
+logger = setup_logger("s3_uploader")
+
+
+def upload_file(s3cli, bucket: str, key: str, file_cnt: bytes):
+    if not is_file_exists(s3cli, len(file_cnt), bucket, key):
+        upload_file_in_mem(s3cli, file_cnt, bucket, key)
+
+
+def is_file_exists(s3_client, fsize: int, bucket: str, key: str) -> bool:
+    try:
+        response = s3_client.head_object(Bucket=bucket, Key=key)
+        resp_code = response["ResponseMetadata"]["HTTPStatusCode"]
+        assert (
+            resp_code == 200
+        ), f"status code is {response['ResponseMetadata']['HTTPStatusCode']}"
+        assert response["ContentLength"] == fsize, "file size mismatch"
+        return True
+    except ClientError as e:
+        if e.response["Error"]["Code"] == "404":
+            return False
+
+        logger.exception(f"head file {bucket}/{key}")
+
+    return False
+
+
+def _get_s3_fname(root, file_name):
+    return file_name[len(root) :].lstrip("/")
+
+
+def connect_s3(server: str, region: str):
+    b3_session = boto3.Session(
+        aws_access_key_id="1", aws_secret_access_key="1", region_name=region
+    )
+    return b3_session.client("s3", endpoint_url=server)
+
+
+def _list_all_files(dir) -> Generator:
+    for root, dirs, files in os.walk(dir):
+        for file in files:
+            yield os.path.join(root, file)
+
+
+def upload_file_in_mem(s3_client, file_cnt: bytes, bucket: str, key:str):
+    """Upload a file in memory(without disk) to an S3 bucket"""
+
+    try:
+        response = s3_client.put_object(Body=file_cnt, Bucket=bucket, Key=key)
+        logger.info(f"uploaded {bucket}/{key}")
+    except ClientError as e:
+        logger.exception(f"upload file {bucket}/{key}")
+        raise e
+
+
+if __name__ == "__main__":
+    s3_cli = connect_s3(
+        "http://100.97.108.34:8333",
+        "home",
+    )
+    with open(r"/home/laisky/repo/laisky/ramjet/README.md", "rb") as f:
+        upload_file(s3_cli, "test", "README.md", f.read())
