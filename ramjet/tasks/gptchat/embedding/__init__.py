6d6c1bcfa2cbb0db85b0bc1178b808ba6c537b07
Laisky <github@laisky.com>
2023-03-29 05:54:32 +0000

    feat: Refactor OpenAI object initialization and outputs storage.
    - Add new parameters to initialize the `OpenAI` object
    - Modify `VectorDBQAWithSourcesChain` initialization to include the new parameters
    - Optimize `all_chains` initialization to store only outputs when querying.

--- ramjet/tasks/gptchat/embedding/__init__.py
+++ ramjet/tasks/gptchat/embedding/__init__.py
@@ -20,7 +20,13 @@ def setup():
     global all_chains
     for project_name, store in load_all_stores().items():
         all_chains[project_name] = VectorDBQAWithSourcesChain.from_llm(
-            llm=OpenAI(temperature=0), vectorstore=store
+            llm=OpenAI(
+                temperature=0,
+                max_tokens=1000,
+                model_name="text-davinci-003",
+                streaming=False,
+            ),
+            vectorstore=store,
         )
 
         logger.info(f"load chain for project: {project_name}")
@@ -33,5 +39,8 @@ async def query(project_name: str, question: str):
 
 
 def _query(project_name: str, question: str) -> Response:
-    resp = all_chains[project_name]({"question": question})
+    resp = all_chains[project_name](
+        {"question": question},
+        return_only_outputs=True,
+    )
     return Response(question=question, text=resp["answer"], url=resp.get("sources", ""))
