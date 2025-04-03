3c06214845e23219946be1ee7e4be275d7ce16a1
Laisky.Cai <github@laisky.com>
2025-04-03 09:47:47 +0000

    refactor: Comment out unused imports and bind_task function in telegram tasks

--- ramjet/tasks/telegram/__init__.py
+++ ramjet/tasks/telegram/__init__.py
@@ -1,15 +1,15 @@
-from ramjet.engines import ioloop, thread_executor
-from ramjet.utils.log import logger as ramjet_logger
+# from ramjet.engines import ioloop, thread_executor
+# from ramjet.utils.log import logger as ramjet_logger
 
-from .notes import run as run_notes
+# from .notes import run as run_notes
 
-logger = ramjet_logger.getChild("tasks.telegram.notes")
+# logger = ramjet_logger.getChild("tasks.telegram.notes")
 
 
-def bind_task():
-    def run():
-        logger.info("running...")
-        thread_executor.submit(run_notes)
-        ioloop.call_later(10 * 60, run)  # every 10 minutes
+# def bind_task():
+#     def run():
+#         logger.info("running...")
+#         thread_executor.submit(run_notes)
+#         ioloop.call_later(10 * 60, run)  # every 10 minutes
 
-    run()
+#     run()
